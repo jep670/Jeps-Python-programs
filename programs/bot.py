6aedfaace715994db1ba73e20922d6342fd2557a
jep670 <53100456+jep670@users.noreply.github.com>
2020-08-12 19:02:22 -0700

    Major bot updates

--- programs/bot.py
+++ programs/bot.py
@@ -1,16 +1,16 @@
 from discord.ext import commands
 import jrlib
 import random
+import discord
 
 bot = commands.Bot(command_prefix='~', case_insensitive=True)
 bot.remove_command('help')
 
 # Variables
 
-carcount = 0
 eggcount = 0
 wordcount = 0
-pingpongcount = 0
+diceCount = 0
 
 # Ping-pong stuff
 
@@ -24,19 +24,20 @@ async def pong(ctx):
     # This confuses the bot
     await ctx.send(f"Po- wait what?")
 
-@bot.command()
-async def pingpong(ctx):
-    # This makes it so you play pingpong with the bot
-    global pingpongcount
-    pingpongcount = pingpongcount + 1
-    await ctx.send(f":ping_pong:")
-
 # Other
 
 @bot.command()
-async def Help(ctx):
+async def help(ctx):
     # Basic help command
-    await ctx.send("``` Prefix: '~' \n \n Other \n help: Shows this message \n captcha: makes sure you're talking with a human \n egg: EGG \n nicecar: Give you a nice car! \n word: Makes a word for you! \n inv: Shows all of the objects collected in the server. \n \n Ping & Pong \n ping: Tests Latency of bot \n pingpong: Play pingpong with the bot! ```")
+    help_menu = "ping - Test bot latency"\
+                "\n help - Opens the help menu"\
+                "\n captcha - Test if you're talking with a bot"\
+                "\n word [length] - Makes a new word with the length of word provided"\
+                "\n egg - EGG"\
+                "\n dice [sides] - Rolls a dice!"\
+                "\n inv - See the inventory of your server"
+    embed = discord.Embed(title = "Help", description = help_menu)
+    await ctx.send(embed = embed)
 
 @bot.command()
 async def captcha(ctx):
@@ -44,40 +45,65 @@ async def captcha(ctx):
     await ctx.send(" ``beep boop i am not a bot`` ")
 
 @bot.command()
-async def word(ctx):
+async def word(ctx, length):
     global wordcount
-    wordcount = wordcount + 1
-    length = random.randint(1, 10)
     
-    outputword = jrlib.generate_word(length)
+    if length.isdigit():
+        length = int(length)
+        if length > 2000:
+            await ctx.send(f'The number you have inputted is above the 2000 character limit.')
+            return
+        outputword = jrlib.generate_word(length)
+        wordcount = wordcount + 1
+        await ctx.send(outputword)
+    elif length.lower() == "a number":
+        await ctx.send(f'The string of text "a number" isn\'t a number ')
+    else:
+        await ctx.send(f"{length} isn't a number stupid")
+    
 
     # Makes a random word for you!
-    await ctx.send(outputword)
 
 @bot.command()
 async def egg(ctx):
     # EGG
     global eggcount
-    eggcount = eggcount + 28
-    await ctx.send(":egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg::egg:")
+    eggcount = eggcount + 1
+    await ctx.send(":egg:")
 
-@bot.command()
-async def nicecar(ctx):
-    # Wow, nice car!
-    global carcount  
-    carcount = carcount + 1
-    await ctx.send(" n||ice ca||r ")
+@bot.command() 
+async def dice(ctx, sides):
+    global diceCount
+    
+    if sides.isdigit():
+        sides = int(sides)
+        if sides < 1:
+            await ctx.send(f"Cannot roll a dice with sides that are less then one.")
+            return
+        output = random.randint(1, sides)
+        diceCount = diceCount + 1
+        await ctx.send(output)
+    elif sides.lower() == "a number":
+        await ctx.send(f'The string of text "a number" isn\'t a number ')
+    else:
+        await ctx.send(f"{sides} isn't a number stupid")
+    # Rolls a Dice
+    
 
 # Inventory
 
 @bot.command()
 async def inv(ctx):
     # Server's inventory
-    global carcount
+    global diceCount
     global eggcount
     global wordcount
-    global pingpongcount
-    await ctx.send(f"The server's inventory contains: ``` {eggcount} egg(s) \n {carcount} car(s) \n {wordcount} word(s) \n {pingpongcount} ping pong round(s) played with the bot. ```")
+
+    inv_menu = f"{eggcount} eggs"\
+               f"\n {diceCount} dice thrown"\
+               f"\n {wordcount} words generated"
+    embed = discord.Embed(title = "Inventory", description = inv_menu)
+    await ctx.send(embed = embed)
 
 file = open("token.txt")  
 token = file.read()
